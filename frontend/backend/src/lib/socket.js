ece686554b36c8ccfaad81870907fb21de36869a
OrsiniBr <udochukwugodwin13@gmail.com>
2025-08-31 13:34:23 +0100

    go back to render deploy

--- frontend/backend/src/lib/socket.js
+++ frontend/backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://trust-let.vercel.app"],
+    origin: ["http://localhost:5173"],
   },
 });
 
--- frontend/frontend/src/lib/axios.js
+++ frontend/frontend/src/lib/axios.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "https://trust-let-w48r.vercel.app/"
-      : "https://trust-let-w48r.vercel.app/api",
-
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
