0fe48576f7da96396b45f54489038e0b50560bd6
OrsiniBr <udochukwugodwin13@gmail.com>
2025-09-02 22:38:28 +0100

    add vercel to both

--- frontend/frontend/src/lib/axios.js
+++ frontend/frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "https://trust-let-blxr.vercel.app/api"
+      : "https://trust-let-blxr.vercel.app/api",
+
   withCredentials: true,
 });
